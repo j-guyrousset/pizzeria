2482285f15531cc1266fce2e2e60b94e5268d83b
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2022-01-03 16:31:16 +0100

    add price calculation

--- src/js/script.js
+++ src/js/script.js
@@ -35,8 +35,8 @@
 
   const classNames = {
     menuProduct: {
-      wrapperActive: '.active',
-      imageVisible: '.active',
+      wrapperActive: 'active',
+      imageVisible: 'active',
     },
   };
 
@@ -59,41 +59,46 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('new Product: ', thisProduct);
     }
 
     renderInMenu(){
       const thisProduct = this;
-
       /*generate text based on template (but not understood as HTML code)*/
       const generatedHTML = templates.menuProduct(thisProduct.data);
-
       /*create element understood as HTML using utils.createElementFromHTML*/
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-
       /*find menu container*/
       const menuContainer = document.querySelector(select.containerOf.menu);
-
       /*add element to menu at this point only the created template gets active*/
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.element.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.active = thisProduct.element.querySelector(select.all.menuProductsActive);
+    }
+
     initAccordion(){
       const thisProduct = this;
-
       /*find the clickable trigger (element that should react to click)*/
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /*START: add event listener to clickable element on event click*/
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /*prevent from default action on event*/
         event.preventDefault();
-
         /*find active products (that have active class)*/
-        const activeProducts = document.querySelectorAll('article' + classNames.menuProduct.wrapperActive);
-
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
         /*if there's an active product and it's not thisProduct.element, removeclass active from it*/
         for (let product of activeProducts){
           const productClass = product.getAttribute('class');
@@ -105,21 +110,131 @@
         thisProduct.element.classList.toggle('active');
       });
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+      console.log('function initOrderForm called', thisProduct.formInputs);
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for (let formInput of thisProduct.formInputs){
+        formInput.addEventListener('change', function(event){
+          event.preventDefault();
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      console.log('function ProcessOrder called on:', thisProduct.id);
+      const formData = utils.serializeFormToObject(thisProduct.form);  // we know what has been chosen on the form
+      console.log('form data: ', formData);
+      console.log('the options choosed in the form are:');
+
+
+      //set price to default defaultValue
+      let price = thisProduct.data.price;
+      console.log('default price: ', price);
+
+      for(let paramId in thisProduct.data.params){
+        const param = thisProduct.data.params[paramId];
+        console.log('parameter ID: ', paramId);
+        console.log('parameter: ', param);  //the table which name is paramId
+
+
+        let elementHTML = '';
+        if (param.type !== 'select'){
+          for(let optionId in param.options){
+            const option = param.options[optionId];
+            console.log('option ID: ', optionId);
+            console.log('option: ', option);  //the table which name is optionId
+            console.log('price of the option: ', option.price);
+            //const optionPrice = option.price;
+
+            elementHTML = document.getElementById(optionId);
+            console.log('elmt HTML: ', elementHTML);
+
+            if(option.default && elementHTML.checked){
+              console.log('default and checked');
+              price += 0;
+            }
+            if(option.default && !(elementHTML.checked)){
+              console.log('default and NOT checked');
+              price -= option.price;
+            }
+            if(!option.default && elementHTML.checked){
+              console.log('NOT default and checked');
+              price += option.price;
+            }
+          }
+        } else {
+          const elem = document.querySelector('select');
+          console.log('select element: ', elem);
+          const elementHTMLs = elem.querySelectorAll('option');
+          for (let optionElement of elementHTMLs){
+            elementHTML = optionElement;
+            console.log('other elmt HTML: ', optionElement);
+            const optionPrice = param.options[elementHTML.value].price;
+            console.log('option price for the element: ', optionPrice);
+
+            if(elementHTML.value == 'standard' && (elementHTML.selected)){
+              console.log('default: ', elementHTML.value == 'standard');
+              console.log('selected: ', elementHTML.selected);
+              console.log('price =  ' + price + '+' + 0);
+              price += 0;
+              console.log('updated price: ', price);
+            } else if(elementHTML.value == 'standard' && !(elementHTML.selected)){
+              console.log('default: ', elementHTML.value == 'standard');
+              console.log('selected: ', elementHTML.selected);
+              console.log('price =  ' + price + '+' - optionPrice);
+              price -= optionPrice;
+              console.log('updated price: ', price);
+            } else if(!(elementHTML.value == 'standard') && elementHTML.selected){
+              console.log('default: ', elementHTML.value == 'standard');
+              console.log('selected: ', elementHTML.selected);
+              console.log('price =  ' + price + '+' + optionPrice);
+              price += optionPrice;
+              console.log('updated price: ', price);
+            } else {
+              console.log('default: ', elementHTML.value == 'standard');
+              console.log('selected: ', elementHTML.selected);
+              console.log('price =  ' + price + '+' + 0);
+              price += 0;
+              console.log('updated price: ', price);
+            }
+          }
+        }
+      }
+
+      price = price*formData.amount;
+      console.log('price ', price);
+
+    }
   }
 
   const app = {
     initMenu: function(){
       const thisApp = this;
       console.log('thisApp.data: ', thisApp.data);
-
       for (let productData in thisApp.data.products){
         new Product (productData, thisApp.data.products[productData]);
+        console.log('And the productData is... : ', productData);
+        console.log('which contains : ', thisApp.data.products[productData].description);
       }
     },
 
     initData: function(){
       const thisApp = this;
-
       thisApp.data = dataSource;
     },
 
